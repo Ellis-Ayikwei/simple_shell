ad53e34c53cfaccf846c49a1c148fca646e9dd6f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-12-13 08:54:56 +0000

    'the gates of shell '

--- test/max_pid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Read the maximum process ID value from /proc/sys/kernel/pid_max
-max_pid=$(cat /proc/sys/kernel/pid_max)
-
-# Print the result
-echo "Maximum Process ID Value: $max_pid"
-
