5b08f0f2e31cc029cbb2adbf5787bbfd7e9c3a0e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-12-13 08:05:07 +0000

    'the gates of shell '

--- main.c
+++ main.c
@@ -1,26 +1,44 @@
 #include "shell.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-int main(void) {
-    while (1) {
-        display_prompt();
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-        char** command = read_command();
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-        if (command == NULL || command[0] == NULL) {
-            printf("\n");
-            break;
-        }
-
-        execute_command(command);
-
-        for (int i = 0; command[i] != NULL; i++) {
-            free(command[i]);
-        }
-        free(command);
-    }
-
-    return 0;
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
--- shell_utils_1.c
+++ shell_utils_1.c
@@ -45,21 +45,85 @@ char** read_command() {
 }
 
 void execute_command(char** command) {
-    pid_t pid = fork();
+    // Check if the command is "exit" (built-in)
+    if (strcmp(command[0], "exit") == 0) {
+        // Free allocated memory before exiting
+        for (int i = 0; command[i] != NULL; i++) {
+            free(command[i]);
+        }
+        free(command);
 
-    if (pid == -1) {
-        perror("fork");
+        // Exit the shell
+        exit(EXIT_SUCCESS);
+    }
+
+    // Check if the command is "env" (built-in)
+    if (strcmp(command[0], "env") == 0) {
+        // Print the current environment variables
+        char** env = environ;
+        while (*env != NULL) {
+            printf("%s\n", *env);
+            env++;
+        }
+
+        // Free allocated memory
+        for (int i = 0; command[i] != NULL; i++) {
+            free(command[i]);
+        }
+        free(command);
+
+        return;
+    }
+
+    char* path = getenv("PATH");
+    char* path_copy = strdup(path);
+
+    if (path_copy == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
         exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Child process
-        if (execvp(command[0], command) == -1) {
-            perror("execvp");
+    }
+
+    char* dir = strtok(path_copy, ":");
+    while (dir != NULL) {
+        char* full_path = malloc(strlen(dir) + strlen(command[0]) + 2);
+        if (full_path == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
             exit(EXIT_FAILURE);
         }
-    } else {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
+        strcpy(full_path, dir);
+        strcat(full_path, "/");
+        strcat(full_path, command[0]);
+
+        struct stat st;
+        if (stat(full_path, &st) == 0 && st.st_mode & S_IXUSR) {
+            // The file exists and is executable
+            pid_t pid = fork();
+
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pid == 0) {
+                // Child process
+                if (execv(full_path, command) == -1) {
+                    perror("execv");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                // Parent process
+                int status;
+                waitpid(pid, &status, 0);
+                free(full_path);
+                return;
+            }
+        }
+
+        free(full_path);
+        dir = strtok(NULL, ":");
     }
+
+    // If we reach here, the command doesn't exist in any directory in the PATH
+    printf("Command not found: %s\n", command[0]);
+
+    free(path_copy);
 }
 
